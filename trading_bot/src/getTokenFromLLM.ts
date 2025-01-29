334a4d248384f8d509ea708b4f0fb0061897fef9
AbhilashDash10082k4 <abhilash10082k4@gmail.com>
2025-01-29 23:57:14 +0530

    get token address through regex

--- trading_bot/src/getTokenFromLLM.ts
+++ trading_bot/src/getTokenFromLLM.ts
@@ -1,18 +1,38 @@
-import {HfInference} from "@huggingface/inference";
-import dotenv from 'dotenv';
-export async function getTokenFromLLM(content: string) {
-    //return a token address 
-    // Please install OpenAI SDK first: `npm install openai`
-    const SOL_MEME_BOT = process.env.SOL_MEME_BOT;
-    const inference = new HfInference(SOL_MEME_BOT);
-    const model = "openbmb/MiniCPM-o-2_6";
-    //take the data, procecss in the model and return the result to result var
-    const result = await inference.questionAnswering({
-        
-        data: content,
-        model: model,
-    })
-    console.log(result);
+// import OpenAI from "openai";
+// import dotenv from 'dotenv';
+// dotenv.config();
+// export async function getTokenFromLLM(content: string) {
+//     //return a token address 
+//     // Please install OpenAI SDK first: `npm install openai`
+//     const openai = new OpenAI({
+//         apiKey: process.env.SOL_MEME_BOT
+//     });
+//     console.log("API Key:", process.env.SOL_MEME_BOT);
+//     const completion = await openai.chat.completions.create({
+//         model: "gpt-3.5-turbo-instruct",
+//         messages: [
+//             { role: "system", content: "You are an AI agent that needs to tell me if this tweet is about a solana token. Return me either the address of the solana token or return me null if you can't find a solana token address in this tweet. Just give me the token address and no need to give me anything else." }
+//         ],
+//         store: true,
+//     });
+//     console.log("completion from OpenAi response is ", completion);
+//     console.log("completion.choices[0] from OpenAi response is ",completion.choices[0] )
+//     console.log("completion.choices[0].message from from OpenAi response is ",completion.choices[0].message);
+// }
+// //lang chain, groq, gemini, ollama 
+// /* "You are an AI agent that needs to tell me if this tweet is about a solana token. Return me either the address of the solana token or return me null if you can't find a solana token address in this tweet. Just give me the token address and no need to give me anything else."; */
+
+export function extractSolanaAddress(text: string) {
+    const pattern = /[1-9A-HJ-NP-Za-km-z]{32,44}/g; //regex
+    const matches = text.match(pattern) ?? [];
+    console.log("matches ",matches);
+    for (const match of matches) {
+        if(match == null) {
+            return null;
+        } else {
+            console.log("match ", match)
+            return match;
+        }
+    }
 }
-//lang chain, groq, gemini, ollama 
-/* "You are an AI agent that needs to tell me if this tweet is about a solana token. Return me either the address of the solana token or return me null if you can't find a solana token address in this tweet. Just give me the token address and no need to give me anything else."; */
+extractSolanaAddress("it bonded already not sure it it's going to run much though $ptsd DV7zKmuDX31iGadULENYhFrNgescR3HCDRcAcuXypump")
